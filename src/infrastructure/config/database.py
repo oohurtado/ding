c6cb9159b42f76602a4773c188b8241d20580940
oscar hurtado <raziel_1@hotmail.com>
2026-09-17 18:17:13 -0600

    wip

--- src/infrastructure/config/database.py
+++ src/infrastructure/config/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from config.settings import settings
+from infrastructure.config.settings import settings
 
 engine = create_engine(settings.DATABASE_URI, echo=True, pool_size=20)
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
