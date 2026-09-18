d21434f1e258cc08e3e61098e8bd3504bbc729cb
oscar hurtado <raziel_1@hotmail.com>
2026-09-17 18:48:23 -0600

    wip

--- src/domain/models/entities/user_entit.py
+++ src/domain/models/entities/user_entit.py
@@ -1,5 +1,7 @@
 
-from sqlalchemy import Integer, String
+from datetime import datetime
+
+from sqlalchemy import DateTime, Integer, String, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from infrastructure.config.database import Base
@@ -8,7 +10,8 @@ class UserEntity(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    first_name: Mapped[str] = mapped_column(String(50), unique=False, index=True, nullable=False)
-    last_name: Mapped[str] = mapped_column(String(50), unique=False, index=True, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
     email: Mapped[str] = mapped_column(String(100), unique=True, index=True, nullable=False)
-    password: Mapped[str] = mapped_column(String(100), nullable=False)    
+    password: Mapped[str] = mapped_column(String(100), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, server_default=func.now())
