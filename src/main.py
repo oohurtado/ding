78e8223eec63b54d8588dac05f4a4d30f92e0063
oscar hurtado <raziel_1@hotmail.com>
2026-09-20 10:43:35 -0600

    wip

--- src/main.py
+++ src/main.py
@@ -3,15 +3,19 @@ from dependencies import get_hello_world_handler
 from infrastructure.config.model_registry import register_entities
 from infrastructure.database.database import Base, engine
 from presentation.routers.hello_world_router import hello_world_router
+from presentation.console.menu import init
 
 
-def main() -> None:
+def example() -> None:
     register_entities()
     Base.metadata.create_all(bind=engine)
     request = HelloWorldRequest(name="Oliver")
     response = hello_world_router(get_hello_world_handler(), request)
     print(response.message)
 
+def console() -> None:
+    init()
 
 if __name__ == "__main__":
-    main()
+    # example()
+    console()
